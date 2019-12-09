74c7894306c877b638f10cec4cb21c59cfdd1517
Lucifer <ars9979@icloud.com>
2019-12-09 17:45:56 +0100

    readFileAsAString method implemented in FileReader class

--- TasksManagement/src/FileReader.java
+++ TasksManagement/src/FileReader.java
@@ -1,2 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileReader {
+    public static String readFileAsString(String fileName)throws Exception
+    {
+        String data;
+        data = new String(Files.readAllBytes(Paths.get(fileName)));
+        return data;
+    }
 }
